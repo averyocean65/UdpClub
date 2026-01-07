88d897157ce4b9137da04959916ba66b2df1fa57
averyocean65 <averytoad65@gmail.com>
2026-01-07 17:58:14 +0100

    basic packet class + tests

--- UdpClub/Package.cs
+++ UdpClub/Package.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using UdpClub.Utils;
 
 namespace UdpClub {
 	public class Package {
 		public byte Id;
-		protected byte[] UnhandledData;
+		public byte[] UnhandledData { get; protected set; }
 
 		public Package(byte[] data) {
 			if (data.Length < 1) {
 				throw new ArgumentException(nameof(data));
-				return;
 			}
+
+			Id = data[0];
+			UnhandledData = ArrayUtils.Subarray(data, 1, data.Length - 1);
 			
-			
+			Console.WriteLine($"Unhandled data: {BitConverter.ToString(UnhandledData)}");
 		}
 	}
 }
---
+++ UdpClubTests/PacketTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using UdpClub;
+
+namespace UdpClubTests {
+	[TestFixture]
+	public class PacketTests {
+		[Test]
+		public void CreatePackage() {
+			Package test = new Package(new byte[] { 0x01, 0xFF, 0xAB, 0x0D, 0x00 });
+			Assert.AreEqual(test.UnhandledData, new byte[] { 0xFF, 0xAB, 0x0D, 0x00 });
+		}
+	}
+}
