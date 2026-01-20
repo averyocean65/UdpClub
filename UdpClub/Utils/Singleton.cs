0037319b7fbf265e38b8421dc3e641752092c95c
averyocean65 <averytoad65@gmail.com>
2026-01-20 08:05:51 +0100

    make Singleton public

--- UdpClub/Utils/Singleton.cs
+++ UdpClub/Utils/Singleton.cs
@@ -1,5 +1,17 @@
 namespace UdpClub.Utils {
-    internal class Singleton {
-        
+    public class Singleton<T> where T : new() {
+        private static T _instance;
+        private static readonly object ThreadLock = new object();
+
+        public static T Instance {
+            get {
+                lock (ThreadLock) {
+                    if (_instance == null) {
+                        _instance = new T();
+                    }
+                    return _instance;
+                }
+            }
+        }
     }
 }
